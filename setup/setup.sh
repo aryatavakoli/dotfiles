54cb710dac5d1068dcecd7d3d212828d188ccf0c
Arya Tavakoli <25291801+aryatavakoli@users.noreply.github.com>
2023-02-26 17:54:36 -0800

    Create setup script

---
+++ setup/setup.sh
@@ -0,0 +1,14 @@
+export OS_TYPE=$(uname -s) 
+
+echo $OS_TYPE
+
+if [ $OS_TYPE == "Linux" ]; then 
+    chmod u+x linux.sh 
+    ./linux.sh; 
+fi
+if [ $OS_TYPE == "Darwin" ]; then 
+    chmod u+x mac.sh 
+    ./mac.sh; 
+fi
+
+chsh -s /usr/bin/zsh
