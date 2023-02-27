2b7847d8990f7678c40c3596c05c27cbbb041fe4
Arya Tavakoli <25291801+aryatavakoli@users.noreply.github.com>
2023-02-26 18:13:29 -0800

    Updatee setup script

--- setup/setup.sh
+++ setup/setup.sh
@@ -4,11 +4,11 @@ echo $OS_TYPE
 
 if [ $OS_TYPE == "Linux" ]; then 
     chmod u+x linux.sh 
-    ./linux.sh
+    sh ./linux.sh
 fi
 if [ $OS_TYPE == "Darwin" ]; then 
     chmod u+x mac.sh 
-    ./mac.sh
+    sh ./mac.sh
 fi
 
 chsh -s /usr/bin/zsh
