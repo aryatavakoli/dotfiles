3757366f013dca8da874212ebdcaa15030e694e6
Arya Tavakoli <25291801+aryatavakoli@users.noreply.github.com>
2025-04-16 14:39:26 -0700

    Update mac.sh

--- setup/mac.sh
+++ setup/mac.sh
@@ -3,4 +3,11 @@ xcode-select --install
 NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 brew bundle --file=../brew/Brewfile
 brew bundle --file=../brew/Caskfile
+cd ~/Library/Fonts && { 
+    curl -O -L 'https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Regular.ttf'
+    curl -O -L 'https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold.ttf'
+    curl -O -L 'https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Italic.ttf'
+    curl -O -L 'https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold%20Italic.ttf'
+    cd -; 
+}
 for EXT in $(cat ../vscode/Codefile); do code --install-extension $EXT; done
